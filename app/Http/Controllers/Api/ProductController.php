b5a5c70bb6bc7bf846ff974cc44d11e96d208031
asif2212 <asifmd2212@gamil.com>
2023-08-30 02:11:26 +0600

    Product added successfully to the database using api

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -3,10 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\BaseController as BaseController;
-use App\Http\Controllers\Controller;
 use App\Http\Resources\ProductResource;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Validator;
 
 class ProductController extends BaseController
 {
@@ -15,5 +15,20 @@ class ProductController extends BaseController
         $products = Product::all();
         // return $this->sendResponse($products->toArray(), 'Product Retrieved Successfully');
         return $this->sendResponse(ProductResource::collection($products), 'Product Retrieved Successfully');
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'description' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return $this->sendError('Validation Error', $validator->errors());
+        }
+
+        $product = Product::create($request->all());
+        return $this->sendResponse(new ProductResource($product), 'Product Added Successfully');
     }
 }
